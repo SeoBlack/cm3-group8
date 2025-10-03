2d1dcad29d6e7a53633ec31a660f15a1d97f3fd5
MINH HOANG <quangminh02082006@gmail.com>
2025-10-03 14:31:51 +0300

    testing and implementing signup_login

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -4,14 +4,14 @@ import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const navigate = useNavigate();
-  const email = useField("email");
+  const username = useField("username");
   const password = useField("password");
   const { login, isLoading, error } = useLogin("/api/users/login");
 
   const handleFormSubmit = async (e) => {
     e.preventDefault();
     await login({
-      email: email.value,
+      username: username.value,
       password: password.value,
     });
     if (!error) {
@@ -24,8 +24,8 @@ const Login = () => {
     <div className="create">
       <h2>Login</h2>
       <form onSubmit={handleFormSubmit}>
-        <label>Email address:</label>
-        <input {...email} />
+        <label>Username:</label>
+        <input {...username} />
         <label>Password:</label>
         <input {...password} />
         <button>Login</button>
