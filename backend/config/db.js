b72c054d9db1efa8a66069575be1dac1422918d5
SeoBlack <sooreennadum2001@gmail.com>
2025-10-03 11:00:03 +0300

    no auth first api

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,9 @@ const config = require("../utils/config");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(config.MONGO_URI);
+    const conn = await mongoose.connect(config.MONGO_URI, {
+      dbName: "cm3-group8",
+    });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
--- backend/controllers/jobControllers.js
+++ backend/controllers/jobControllers.js
@@ -4,26 +4,143 @@ const mongoose = require("mongoose");
 //GET / jobs;
 const getAllJobs = async (req, res) => {
   res.send("getAllJobs");
+  //MINH
 };
 
 // POST /jobs
 const createJob = async (req, res) => {
-  res.send("createJob");
+  console.log(req.body);
+  const {
+    title,
+    type,
+    description,
+    companyName,
+    contactEmail,
+    contactPhone,
+    website,
+    size,
+    location,
+    salary,
+    experienceLevel,
+    postedDate,
+    status,
+    applicationDeadline,
+    requirements,
+  } = req.body;
+  if (
+    !title ||
+    !type ||
+    !description ||
+    !companyName ||
+    !contactEmail ||
+    !contactPhone
+  ) {
+    return res.status(400).json({ error: " fields are missing" });
+  }
+  try {
+    const job = await Job.create({
+      title,
+      type,
+      description,
+      company: {
+        name: companyName,
+        contactEmail: contactEmail,
+        contactPhone: contactPhone,
+        website: website,
+        size: size,
+      },
+      location,
+      salary,
+      experienceLevel,
+      postedDate,
+      status,
+      applicationDeadline,
+      requirements,
+    });
+    res.status(201).json(job);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 // GET /jobs/:jobId
 const getJobById = async (req, res) => {
-  res.send("getJobById");
+  const { jobId } = req.params;
+  if (!jobId) {
+    return res.status(400).json({ error: "Job ID is required" });
+  }
+  try {
+    const job = await Job.findById(jobId);
+    res.status(200).json(job);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 // PUT /jobs/:jobId
 const updateJob = async (req, res) => {
-  res.send("updateJob");
+  const { jobId } = req.params;
+  const {
+    title,
+    type,
+    description,
+    companyName,
+    contactEmail,
+    contactPhone,
+    website,
+    size,
+    location,
+    salary,
+    experienceLevel,
+    postedDate,
+    status,
+    applicationDeadline,
+    requirements,
+  } = req.body;
+  if (
+    !title ||
+    !type ||
+    !description ||
+    !companyName ||
+    !contactEmail ||
+    !contactPhone
+  ) {
+    return res.status(400).json({ error: "All fields are required" });
+  }
+  try {
+    const job = await Job.findByIdAndUpdate(
+      jobId,
+      {
+        title,
+        type,
+        description,
+        company: {
+          name: companyName,
+          contactEmail: contactEmail,
+          contactPhone: contactPhone,
+          website: website,
+          size: size,
+        },
+        location,
+        salary,
+        experienceLevel,
+        postedDate,
+        status,
+        applicationDeadline,
+        requirements,
+      },
+      { new: true }
+    );
+    res.status(200).json(job);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 // DELETE /jobs/:jobId
 const deleteJob = async (req, res) => {
   res.send("deleteJob");
+  //Minh
 };
 
 module.exports = {
